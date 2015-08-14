065f56436497ca104d3ddb6b98d3956528a6a9e2
warpspak <warpspak@gmail.com>
2015-08-14 15:51:34 -0300

    Initial Commit

---
+++ AndrePedro.LojaVirtual.UnitTest/UnitTestAndrePedro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AndrePedro.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestAndrePedro
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
